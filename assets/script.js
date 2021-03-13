cd130465fafa6b39175a39f5cc414ef368594a1c
El Roble <san_lucas2005@yahoo.com>
2021-03-12 20:41:38 -0700

    Header, navigation top and bottom and footer backgrounds changed

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,16 @@ checkbox.onclick = function() {myFunction()};
 function myFunction() {
   if(checkbox.checked) { 
     document.getElementById("top-nav").style.backgroundColor = "#397bb8";
+    document.getElementById("header").style.backgroundColor = "#4dd1e9";
     document.getElementById("side-bar").style.backgroundColor = "#397bb8";
-    document.getElementById("footer").style.backgroundColor = "#397bb8";
+    document.getElementById("footer").style.backgroundColor = "#4dd1e9";
     document.getElementById("bottom-nav").style.backgroundColor = "#397bb8";
   }
   else {          
-    document.getElementById("top-nav").style.backgroundColor = "#042b50";
+    document.getElementById("top-nav").style.backgroundColor = "#4dd1e9";
+    document.getElementById("header").style.backgroundColor = "#397bb8";
     document.getElementById("side-bar").style.backgroundColor = "#042b50";
-    document.getElementById("footer").style.backgroundColor = "#042b50";
-    document.getElementById("bottom-nav").style.backgroundColor = "#042b50";
+    document.getElementById("footer").style.backgroundColor = "#397bb8";
+    document.getElementById("bottom-nav").style.backgroundColor = "#4dd1e9";
   }
 }

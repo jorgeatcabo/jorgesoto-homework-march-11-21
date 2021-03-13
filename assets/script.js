ee1631d89c4f9a5011ee74fa9e81fe87fac90bfd
El Roble <san_lucas2005@yahoo.com>
2021-03-12 18:17:50 -0700

    Scroll added to the sidebar

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,15 @@ checkbox.onclick = function() {myFunction()};
 
 function myFunction() {
   if(checkbox.checked) { 
-    document.getElementById("topnav").style.backgroundColor = "#397bb8";
-    document.getElementById("sidebar").style.backgroundColor = "#397bb8";
+    document.getElementById("top-nav").style.backgroundColor = "#397bb8";
+    document.getElementById("side-bar").style.backgroundColor = "#397bb8";
     document.getElementById("footer").style.backgroundColor = "#397bb8";
-    document.getElementById("bottomnav").style.backgroundColor = "#397bb8";
+    document.getElementById("bottom-nav").style.backgroundColor = "#397bb8";
   }
   else {          
-    document.getElementById("topnav").style.backgroundColor = "#042b50";
-    document.getElementById("sidebar").style.backgroundColor = "#042b50";
+    document.getElementById("top-nav").style.backgroundColor = "#042b50";
+    document.getElementById("side-bar").style.backgroundColor = "#042b50";
     document.getElementById("footer").style.backgroundColor = "#042b50";
-    document.getElementById("bottomnav").style.backgroundColor = "#042b50";
+    document.getElementById("bottom-nav").style.backgroundColor = "#042b50";
   }
 }
